4d3e2e73474e1c8232477eeee4b7da7ab7cc8f55
giorgi <giorgikochuashvili2002@gmail.com>
2023-01-05 20:40:16 +0400

    active class added

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 import userPhoto from '../images/image-jeremy.png';
 import { useGlobalContext } from './context';
 function User() {
-  const { displayPage } = useGlobalContext();
+  const { time, displayPage } = useGlobalContext();
   return (
     <div className='profile-container'>
       <div className='user-info-container'>
@@ -13,13 +13,22 @@ function User() {
         </div>
       </div>
       <div className='time-select-container'>
-        <button className='btn-time active' onClick={(e) => displayPage(e)}>
+        <button
+          className={time === 'daily' ? 'btn-time active' : 'btn-time'}
+          onClick={(e) => displayPage(e)}
+        >
           Daily
         </button>
-        <button className='btn-time' onClick={(e) => displayPage(e)}>
+        <button
+          className={time === 'weekly' ? 'btn-time active' : 'btn-time'}
+          onClick={(e) => displayPage(e)}
+        >
           Weekly
         </button>
-        <button className='btn-time' onClick={(e) => displayPage(e)}>
+        <button
+          className={time === 'monthly' ? 'btn-time active' : 'btn-time'}
+          onClick={(e) => displayPage(e)}
+        >
           Monthly
         </button>
       </div>
